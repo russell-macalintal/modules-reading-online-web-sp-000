4ae03858e11dc719c623c70cf5ec2d510e29cdf6
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-29 01:43:34 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,6 +2,7 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
+  extend MetaDancing
   include Dance
 
   attr_accessor :name
@@ -9,5 +10,5 @@ class Dancer
   def initialize(name)
     @name = name
   end
-
+  
 end
