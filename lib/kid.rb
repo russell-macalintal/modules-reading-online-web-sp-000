8e32611c8265a3ec547f158c8ed752384394984b
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-29 01:47:57 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,13 +2,15 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  extend MetaDancing
-  include Dance
+  # extend MetaDancing
+  # include Dance
+  # 
+  # attr_accessor :name
+  # 
+  # def initialize(name)
+  #   @name = name
+  # end
 
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
-
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 end
